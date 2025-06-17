8f44bba35a9bef4537e487f1b1eb4eb93d437de2
Ramon Barbosa Souza <ramon.barbosa@w5i.com.br>
2025-06-17 11:00:39 -0300

    feat: mudando o web socket

--- src/app/environments/environment.ts
+++ src/app/environments/environment.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: false,
-  apiUrl: 'http://localhost:8080/syncdb',
+  apiUrl: 'http://localhost:8081/syncdb',
   // apiUrl: 'https://syncdb-mfa3.onrender.com/syncdb',
-  apiUrlWebSocket: 'localhost:8080/syncdb',
+  apiUrlWebSocket: '//localhost:8081/syncdb/socket',
   // apiUrlWebSocket: 'syncdb-mfa3.onrender.com/syncdb',
 };
