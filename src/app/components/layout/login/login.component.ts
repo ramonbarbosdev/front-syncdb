7420739c6a2b75cc16a5e6ed88b72913591005ec
Ramon Barbosa Souza <ramon.barbosa@w5i.com.br>
2025-07-10 15:04:15 -0300

    feat: adicionando carregamento

--- src/app/components/layout/login/login.component.ts
+++ src/app/components/layout/login/login.component.ts
@@ -5,17 +5,18 @@ import { AuthService } from '../../../auth/auth.service';
 import Swal from 'sweetalert2';
 import { InputPasswordComponent } from '../../component/input-password/input-password.component';
 import { InputTextComponent } from '../../component/input-text/input-text.component';
-import { ButtonComponent } from '../../component/button/button.component';
 import { WebsocketService } from '../../../services/websocket.service';
-
+import { HlmSpinnerComponent } from '@spartan-ng/helm/spinner';
+import { CommonModule } from '@angular/common';
 @Component({
   selector: 'app-login',
   imports: [
+    CommonModule,
     FormsModule,
     InputPasswordComponent,
     InputTextComponent,
-    ButtonComponent,
     RouterModule,
+    HlmSpinnerComponent,
   ],
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss',
@@ -27,22 +28,28 @@ export class LoginComponent {
     senha: '',
   };
 
+  loading = false;
+
   router = inject(Router);
   websocketService = inject(WebsocketService);
 
   constructor(private auth: AuthService) {}
 
   logar() {
+    this.loading = true
+
     this.auth.login(this.objeto).subscribe({
       next: (res: any) => {
         this.websocketService
           .connect()
           .then(() => {
             console.log('[Login OK + WebSocket conectado]');
+            this.loading = false;
             this.auth.setToken(res.Authorization);
             this.router.navigate(['admin/dashboard']);
           })
           .catch((err) => {
+            this.loading = false;
             console.error('[Erro ao conectar WebSocket após login]', err);
             Swal.fire({
               icon: 'error',
@@ -52,6 +59,7 @@ export class LoginComponent {
           });
       },
       error: (err) => {
+        this.loading = false;
         console.log(err);
         Swal.fire({
           icon: 'error',
